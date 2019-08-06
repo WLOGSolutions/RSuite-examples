033e9e4f899acca20550cc37bfe771e19c685bb9
Wit Jakuczun <wit.jakuczun@wlogsolutions.com>
2019-08-06 15:34:52 +0200

    Python: created project

---
+++ Python-rsuite/packages/extdeps/R/package_logger.R
@@ -0,0 +1,31 @@
+#----------------------------------------------------------------------------
+# extdeps
+#
+# Package logger setup
+#----------------------------------------------------------------------------
+
+.logger_name <- "extdeps"
+.pkg_logger <- logging::getLogger(.logger_name)
+.pkg_logger$setLevel("FINEST")
+
+pkg_loginfo <- function(msg, ...) tryCatch(logging::loginfo(msg, ..., logger = .pkg_logger),
+                                           error = function(e) warning(e))
+pkg_logdebug <- function(msg, ...) tryCatch(logging::logdebug(msg, ..., logger = .pkg_logger),
+                                            error = function(e) warning(e))
+pkg_logerror <- function(msg, ...) tryCatch(logging::logerror(msg, ..., logger = .pkg_logger),
+                                            error = function(e) warning(e))
+pkg_logwarn <- function(msg, ...) tryCatch(logging::logwarn(msg, ..., logger = .pkg_logger),
+                                           error = function(e) warning(e))
+pkg_logfinest <- function(msg, ...) tryCatch(logging::logfinest(msg, ..., logger = .pkg_logger),
+                                             error = function(e) warning(e))
+
+#'
+#' Retrieves extdeps logger.
+#' 
+#' @return logger object
+#' 
+#' @export
+#' 
+extdeps_getLogger <- function() {
+  .pkg_logger
+}
